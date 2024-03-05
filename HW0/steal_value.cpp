9a6c347bc9a978fcd22ef02c01f330870f90fd12
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-03-05 14:05:17 +0800

    ac version

--- HW0/steal_value.cpp
+++ HW0/steal_value.cpp
@@ -2,23 +2,18 @@
 #include <vector>
 using namespace std;
 
-int N,K;
-
-
 int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N,K;
     cin>>N>>K;
-    // int dp[1005] = {0};
-    // int weight[105] = {0};
-    // int value[105] = {0};
     vector<int> dp(K+1);
     vector<int> weight(N);
     vector<int> value(N);
 
     for(int i = 0;i<N;i++){
         cin>>value[i]>>weight[i];
-    }
-
-    for(int i = 0;i<N;i++){
         for(int j = K;j-weight[i]>=0;j--){
             dp[j] = max(dp[j],dp[j-weight[i]]+value[i]);
         }
