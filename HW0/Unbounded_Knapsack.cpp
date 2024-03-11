3802349cd1a9f99c379eb899728198041b4cb51f
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-03-11 17:36:11 +0800

    unbounded knspack

--- HW0/Unbounded_Knapsack.cpp
+++ HW0/Unbounded_Knapsack.cpp
@@ -6,17 +6,17 @@ int main(void){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int N,K;
-    cin>>N>>K;
-    vector<int> dp(K+1);
+    int N,W;
+    cin>>N>>W;
+    vector<int> dp(W+1);
     vector<int> weight(N);
     vector<int> value(N);
 
     for(int i = 0;i<N;i++){
-        cin>>value[i]>>weight[i];
-        for(int j = K;j-weight[i]>=0;j--){
+        for(int j = weight[i];j<=W;j++){
             dp[j] = max(dp[j],dp[j-weight[i]]+value[i]);
         }
     }
-    cout<<dp[K]<<"\n";
+
+    cout<<dp[W]<<"\n";
 }
