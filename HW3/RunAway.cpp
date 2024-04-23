366fc62b9a1055b2368b8d813dc70fc35c7fb0d7
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-04-24 01:48:32 +0800

    check not good

--- HW3/RunAway.cpp
+++ HW3/RunAway.cpp
@@ -4,19 +4,32 @@ using namespace std;
 const string Add = "Add";
 const string Delete = "Delete";
 const string Check = "Check";
-
+int result = 0;
 class Node{
     private:
         int val;
+        int a;
         int distance;
         int child_amount;
+        int max;
     public:
+        int d1,d2;
         Node* parent;
         vector<Node*> child;
         Node(int v,int d){
             child_amount = 0;
             val = v;
+            a = val;
             distance = d;
+            d1 = 0;
+            d2 = 0;
+            max = 0;
+        }
+        int getA(){
+            return a;
+        }
+        int setA(int x){
+            a = x;
         }
         int getVal(){
             return val;
@@ -26,6 +39,18 @@ class Node{
         }
         void setDis(int a){
             distance = a;
+        }
+        int getChildAmount(){
+            return child_amount;
+        }
+        void setD1(int a){
+            d1 = a;
+        }
+        void setD2(int a){
+            d2 = a;
+        }
+        void setMax(){
+            max = val+d1+d2;
         }
         void addChild(Node* node){
             child_amount++;
@@ -48,7 +73,7 @@ class Node{
         }
         void printNode(){
             cout<<"I am "<<val<<" with distance : "<<distance<<"\n";
-            cout<<"My parent:\n";
+            cout<<"My parent: ";
             if(parent == NULL) cout<<"I am root\n";
             else cout<<parent->val<<"\n";
             cout<<"My child:\n";
@@ -62,68 +87,76 @@ class Node{
 class City{
     private:
         vector<Node*> city;
+        vector<int> node_index;
         int amount;
+        Node* root;
     public:
         City(int index,int dis){
+            city.resize(10050,NULL);
+            node_index.push_back(index);
             Node *node = new Node(index,dis);
             node->parent = NULL;
-            city.push_back(node);
-            amount = 1;
+            city[index] = node;
+            amount++;
+            root = node;
         }
         void addNode(int par,int val,int dis){
-            bool exist = false;
             Node* node;
-            for(int i = 0;i<amount;i++){
-                if(city[i]->getVal() == val){
-                    city[i]->setDis(dis);
-                    exist = true;
-                    node = city[i];
-                    break;
-                }
+            if(city[val]!=NULL){
+                node = city[val];
+                node->setDis(dis);
             }
-            if(!exist) node = new Node(val,dis);
-            
-            bool found = false;
-            for(int i = 0;i<amount;i++){
-                if(city[i]->getVal() == par){
-                    node->parent = city[i];
-                    city[i]->addChild(node);
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                Node* parent_node = new Node(par,0);
-                node->parent = parent_node;
-                parent_node->addChild(node);
-                city.push_back(parent_node);
+            else{
+                node = new Node(val,dis);
+                city[val] = node;
                 amount++;
+                node_index.push_back(val);
             }
 
-            if(!exist){
-                city.push_back(node);
+            if(city[par]!=NULL){
+                city[par]->addChild(node);
+                node->parent = city[par];
+            }
+            else{
+                city[par] = new Node(par,0);
+                city[par]->addChild(node);
+                node->parent = city[par];
                 amount++;
-            }   
+                node_index.push_back(par);
+            }
         }
         void deleteNode(int val){
-            for(int i = 0;i<amount;i++){
-                if(city[i]->getVal() == val){
-                    city[i]->setParent(city[i]->parent);
-                    city[i]->parent->deleteChild(city[i]);
-                    city.erase(city.begin()+i,city.begin()+i+1);
-                    amount--;
-                    return;
-                }
-            }
-            return;
+            if(city[val] == NULL) return;
+            city[val]->setParent(city[val]->parent);
+            city[val]->parent->deleteChild(city[val]);
+            Node* tmp = city[val];
+            delete tmp;
+            city[val] = NULL;
+            amount--;
         }
         void printCity(){
-            for(int i = 0;i<amount;i++){
-                city[i]->printNode();
+            for(int i = 0;i<10050;i++){
+                if(city[i]) city[i]->printNode();
             }
         }
+        void solve(Node* current,Node* prev){
+            for(int i = 0;i<current->getChildAmount();i++){
+                if(current == prev) continue;
+                solve(current->child[i],current);
+                if(current->child[i]->getA() > current->d1){
+                    current->setD2(current->d1);
+                    current->setD1(current->child[i]->getA());
+                }
+                else{
+                    current->setD2(max(current->d2,current->child[i]->getA()));
+                }
+            }
+            result = max(result,current->getA()+current->d1+current->d2);
+            current->setA(current->getA()+current->d1);
+        }
         void check(){
-            return;
+            solve(root,NULL);
+            cout<<result;
         }
 };
 
@@ -155,7 +188,7 @@ int main(void){
         else if(command == Check){
             c.check();
         }
-        c.printCity();
+        //c.printCity();
     }
     
 }
